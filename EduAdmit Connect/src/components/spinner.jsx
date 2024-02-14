a8bb514537237184273d1191acbd03b38b7b0d72
BALAMURUGAN M <100469671+mbalamurugan123@users.noreply.github.com>
2024-02-14 12:55:26 +0530

    Delete EduAdmit Connect directory

--- EduAdmit Connect/src/components/spinner.jsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from 'rsuite';
-
-const Spinner=()=>{
-  return(
-    <Loader content="Loading..." />
-    
-  );
-}
-export default Spinner;
--- EduAdmit Connect/src/services/Auth.jsx
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import instance from "./Axios";
-
-const api_url="http://localhost:8181";
-
-export const signIn=(data)=> axios.post(`${api_url}/api/connect/auth/login`,data);
-export const signUp=(data)=> axios.post(`${api_url}/api/connect/auth/register`,data);
